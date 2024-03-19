wowsims/wotlk

Created DB section to house the mapping between random suffix IDs and stat values, and populated it by parsing the Wowhead gear planner DB.
 On branch sod-fork

 Changes to be committed:
	modified:   assets/database/db.bin
	modified:   assets/database/db.json
	modified:   assets/database/leftover_db.json
	modified:   proto/common.proto
	modified:   proto/ui.proto
	modified:   tools/database/database.go
	modified:   tools/database/gen_db/main.go
	modified:   tools/database/wowhead_db.go

tools/database/database.go
@@ -29,9 +29,10 @@ func EnchantToDBKey(enchant *proto.UIEnchant) EnchantDBKey {
 }
 
 type WowDatabase struct {
-	Items    map[int32]*proto.UIItem
-	Enchants map[EnchantDBKey]*proto.UIEnchant
-	Runes    map[int32]*proto.UIRune
+	Items          map[int32]*proto.UIItem
+	RandomSuffixes map[int32]*proto.ItemRandomSuffix
+	Enchants       map[EnchantDBKey]*proto.UIEnchant
+	Runes          map[int32]*proto.UIRune
 
 	Zones map[int32]*proto.UIZone
 	Npcs  map[int32]*proto.UINPC
@@ -52,11 +53,12 @@ func init() {
 
 func NewWowDatabase() *WowDatabase {
 	return &WowDatabase{
-		Items:    make(map[int32]*proto.UIItem),
-		Enchants: make(map[EnchantDBKey]*proto.UIEnchant),
-		Runes:    make(map[int32]*proto.UIRune),
-		Zones:    make(map[int32]*proto.UIZone),
-		Npcs:     make(map[int32]*proto.UINPC),
+		Items:          make(map[int32]*proto.UIItem),
+		RandomSuffixes: make(map[int32]*proto.ItemRandomSuffix),
+		Enchants:       make(map[EnchantDBKey]*proto.UIEnchant),
+		Runes:          make(map[int32]*proto.UIRune),
+		Zones:          make(map[int32]*proto.UIZone),
+		Npcs:           make(map[int32]*proto.UINPC),
 
 		ItemIcons:  make(map[int32]*proto.IconData),
 		SpellIcons: make(map[int32]*proto.IconData),
@@ -65,11 +67,12 @@ func NewWowDatabase() *WowDatabase {
 
 func (db *WowDatabase) Clone() *WowDatabase {
 	return &WowDatabase{
-		Items:    maps.Clone(db.Items),
-		Enchants: maps.Clone(db.Enchants),
-		Runes:    maps.Clone(db.Runes),
-		Zones:    maps.Clone(db.Zones),
-		Npcs:     maps.Clone(db.Npcs),
+		Items:          maps.Clone(db.Items),
+		RandomSuffixes: maps.Clone(db.RandomSuffixes),
+		Enchants:       maps.Clone(db.Enchants),
+		Runes:          maps.Clone(db.Runes),
+		Zones:          maps.Clone(db.Zones),
+		Npcs:           maps.Clone(db.Npcs),
 
 		ItemIcons:  maps.Clone(db.ItemIcons),
 		SpellIcons: maps.Clone(db.SpellIcons),
@@ -235,14 +238,15 @@ func (db *WowDatabase) ToUIProto() *proto.UIDatabase {
 	})
 
 	return &proto.UIDatabase{
-		Items:      mapToSlice(db.Items),
-		Enchants:   enchants,
-		Runes:      mapToSlice(db.Runes),
-		Encounters: db.Encounters,
-		Zones:      mapToSlice(db.Zones),
-		Npcs:       mapToSlice(db.Npcs),
-		ItemIcons:  mapToSlice(db.ItemIcons),
-		SpellIcons: mapToSlice(db.SpellIcons),
+		Items:          mapToSlice(db.Items),
+		RandomSuffixes: mapToSlice(db.RandomSuffixes),
+		Enchants:       enchants,
+		Runes:          mapToSlice(db.Runes),
+		Encounters:     db.Encounters,
+		Zones:          mapToSlice(db.Zones),
+		Npcs:           mapToSlice(db.Npcs),
+		ItemIcons:      mapToSlice(db.ItemIcons),
+		SpellIcons:     mapToSlice(db.SpellIcons),
 	}
 }
 
@@ -266,12 +270,13 @@ func ReadDatabaseFromJson(jsonStr string) *WowDatabase {
 	}
 
 	return &WowDatabase{
-		Items:      sliceToMap(dbProto.Items),
-		Enchants:   enchants,
-		Zones:      sliceToMap(dbProto.Zones),
-		Npcs:       sliceToMap(dbProto.Npcs),
-		ItemIcons:  sliceToMap(dbProto.ItemIcons),
-		SpellIcons: sliceToMap(dbProto.SpellIcons),
+		Items:          sliceToMap(dbProto.Items),
+		RandomSuffixes: sliceToMap(dbProto.RandomSuffixes),
+		Enchants:       enchants,
+		Zones:          sliceToMap(dbProto.Zones),
+		Npcs:           sliceToMap(dbProto.Npcs),
+		ItemIcons:      sliceToMap(dbProto.ItemIcons),
+		SpellIcons:     sliceToMap(dbProto.SpellIcons),
 	}
 }
 
@@ -301,6 +306,8 @@ func (db *WowDatabase) WriteJson(jsonFilePath string) {
 	buffer.WriteString("{\n")
 
 	tools.WriteProtoArrayToBuffer(uidb.Items, buffer, "items")
+	buffer.WriteString(",\n")
+	tools.WriteProtoArrayToBuffer(uidb.RandomSuffixes, buffer, "randomSuffixes")
 	buffer.WriteString(",\n")
 	tools.WriteProtoArrayToBuffer(uidb.Enchants, buffer, "enchants")
 	buffer.WriteString(",\n")

tools/database/wowhead_db.go
@@ -49,15 +49,101 @@ func ParseWowheadDB(dbContents string) WowheadDatabase {
 				log.Fatalf("failed to parse wowhead item db to json %s\n\n%s", err, dbContents[0:30])
 			}
 		}
+
+		if dbName == "wow.gearPlanner.classic.randomEnchant" {
+			standardized, err := hujson.Standardize([]byte(dbContents)) // Removes invalid JSON, such as trailing commas
+			if err != nil {
+				log.Fatalf("Failed to standardize json %s\n\n%s\n\n%s", err, dbContents[0:30], dbContents[len(dbContents)-30:])
+			}
+
+			err = json.Unmarshal(standardized, &wowheadDB.RandomSuffixes)
+			if err != nil {
+				log.Fatalf("failed to parse wowhead random suffix db to json %s\n\n%s", err, dbContents[0:30])
+			}
+		}
 	}
 
 	fmt.Printf("\n--\nWowhead DB items loaded: %d\n--\n", len(wowheadDB.Items))
+	fmt.Printf("\n--\nWowhead DB random suffixes loaded: %d\n--\n", len(wowheadDB.RandomSuffixes))
 
 	return wowheadDB
 }
 
 type WowheadDatabase struct {
-	Items map[string]WowheadItem
+	Items          map[string]WowheadItem
+	RandomSuffixes map[string]WowheadRandomSuffix
+}
+
+type WowheadRandomSuffix struct {
+	ID    int32                    `json:"id"`
+	Name  string                   `json:"name"`
+	Stats WowheadRandomSuffixStats `json:"stats"`
+}
+
+type WowheadRandomSuffixStats struct {
+	Armor             int32 `json:"armor"`
+	Strength          int32 `json:"str"`
+	Agility           int32 `json:"agi"`
+	Stamina           int32 `json:"sta"`
+	Intellect         int32 `json:"int"`
+	Spirit            int32 `json:"spi"`
+	SpellPower        int32 `json:"spldmg"`
+	ArcanePower       int32 `json:"arcsplpwr"`
+	FirePower         int32 `json:"firsplpwr"`
+	FrostPower        int32 `json:"frosplpwr"`
+	HolyPower         int32 `json:"holsplpwr"`
+	NaturePower       int32 `json:"natsplpwr"`
+	ShadowPower       int32 `json:"shasplpwr"`
+	MeleeCrit         int32 `json:"mlecritstrkpct"`
+	MP5               int32 `json:"manargn"`
+	AttackPower       int32 `json:"mleatkpwr"`
+	RangedAttackPower int32 `json:"rgdatkpwr"`
+	Defense           int32 `json:"def"`
+	Block             int32 `json:"blockpct"`
+	Dodge             int32 `json:"dodgepct"`
+	ArcaneResistance  int32 `json:"arcres"`
+	FireResistance    int32 `json:"firres"`
+	FrostResistance   int32 `json:"frores"`
+	NatureResistance  int32 `json:"natres"`
+	ShadowResistance  int32 `json:"shares"`
+	Healing           int32 `json:"splheal"`
+}
+
+func (wrs WowheadRandomSuffix) ToProto() *proto.ItemRandomSuffix {
+	stats := Stats{
+		proto.Stat_StatArmor:             float64(wrs.Stats.Armor),
+		proto.Stat_StatStrength:          float64(wrs.Stats.Strength),
+		proto.Stat_StatAgility:           float64(wrs.Stats.Agility),
+		proto.Stat_StatStamina:           float64(wrs.Stats.Stamina),
+		proto.Stat_StatIntellect:         float64(wrs.Stats.Intellect),
+		proto.Stat_StatSpirit:            float64(wrs.Stats.Spirit),
+		proto.Stat_StatSpellPower:        float64(wrs.Stats.SpellPower),
+		proto.Stat_StatArcanePower:       float64(wrs.Stats.ArcanePower),
+		proto.Stat_StatFirePower:         float64(wrs.Stats.FirePower),
+		proto.Stat_StatFrostPower:        float64(wrs.Stats.FrostPower),
+		proto.Stat_StatHolyPower:         float64(wrs.Stats.HolyPower),
+		proto.Stat_StatNaturePower:       float64(wrs.Stats.NaturePower),
+		proto.Stat_StatShadowPower:       float64(wrs.Stats.ShadowPower),
+		proto.Stat_StatMeleeCrit:         float64(wrs.Stats.MeleeCrit),
+		proto.Stat_StatMP5:               float64(wrs.Stats.MP5),
+		proto.Stat_StatAttackPower:       float64(wrs.Stats.AttackPower),
+		proto.Stat_StatRangedAttackPower: float64(wrs.Stats.RangedAttackPower),
+		proto.Stat_StatDefense:           float64(wrs.Stats.Defense),
+		proto.Stat_StatBlock:             float64(wrs.Stats.Block),
+		proto.Stat_StatDodge:             float64(wrs.Stats.Dodge),
+		proto.Stat_StatArcaneResistance:  float64(wrs.Stats.ArcaneResistance),
+		proto.Stat_StatFireResistance:    float64(wrs.Stats.FireResistance),
+		proto.Stat_StatFrostResistance:   float64(wrs.Stats.FrostResistance),
+		proto.Stat_StatNatureResistance:  float64(wrs.Stats.NatureResistance),
+		proto.Stat_StatShadowResistance:  float64(wrs.Stats.ShadowResistance),
+		proto.Stat_StatHealing:           float64(wrs.Stats.Healing),
+	}
+
+	return &proto.ItemRandomSuffix{
+		Id:    wrs.ID,
+		Name:  wrs.Name,
+		Stats: toSlice(stats),
+	}
 }
 
 type WowheadItem struct {

wowsims/cata

tools/database/database.go
@@ -29,9 +29,10 @@ func EnchantToDBKey(enchant *proto.UIEnchant) EnchantDBKey {
 }
 
 type WowDatabase struct {
-	Items    map[int32]*proto.UIItem
-	Enchants map[EnchantDBKey]*proto.UIEnchant
-	Gems     map[int32]*proto.UIGem
+	Items          map[int32]*proto.UIItem
+	RandomSuffixes map[int32]*proto.ItemRandomSuffix
+	Enchants       map[EnchantDBKey]*proto.UIEnchant
+	Gems           map[int32]*proto.UIGem
 
 	Zones map[int32]*proto.UIZone
 	Npcs  map[int32]*proto.UINPC
@@ -46,11 +47,12 @@ type WowDatabase struct {
 
 func NewWowDatabase() *WowDatabase {
 	return &WowDatabase{
-		Items:    make(map[int32]*proto.UIItem),
-		Enchants: make(map[EnchantDBKey]*proto.UIEnchant),
-		Gems:     make(map[int32]*proto.UIGem),
-		Zones:    make(map[int32]*proto.UIZone),
-		Npcs:     make(map[int32]*proto.UINPC),
+		Items:          make(map[int32]*proto.UIItem),
+		RandomSuffixes: make(map[int32]*proto.ItemRandomSuffix),
+		Enchants:       make(map[EnchantDBKey]*proto.UIEnchant),
+		Gems:           make(map[int32]*proto.UIGem),
+		Zones:          make(map[int32]*proto.UIZone),
+		Npcs:           make(map[int32]*proto.UINPC),
 
 		ItemIcons:    make(map[int32]*proto.IconData),
 		SpellIcons:   make(map[int32]*proto.IconData),
@@ -60,11 +62,13 @@ func NewWowDatabase() *WowDatabase {
 
 func (db *WowDatabase) Clone() *WowDatabase {
 	return &WowDatabase{
-		Items:        maps.Clone(db.Items),
-		Enchants:     maps.Clone(db.Enchants),
-		Gems:         maps.Clone(db.Gems),
-		Zones:        maps.Clone(db.Zones),
-		Npcs:         maps.Clone(db.Npcs),
+		Items:          maps.Clone(db.Items),
+		RandomSuffixes: maps.Clone(db.RandomSuffixes),
+		Enchants:       maps.Clone(db.Enchants),
+		Gems:           maps.Clone(db.Gems),
+		Zones:          maps.Clone(db.Zones),
+		Npcs:           maps.Clone(db.Npcs),
+		
 		ItemIcons:    maps.Clone(db.ItemIcons),
 		SpellIcons:   maps.Clone(db.SpellIcons),
 		ReforgeStats: maps.Clone(db.ReforgeStats),
@@ -206,16 +210,17 @@ func (db *WowDatabase) ToUIProto() *proto.UIDatabase {
 	})
 
 	return &proto.UIDatabase{
-		Items:        mapToSlice(db.Items),
-		Enchants:     enchants,
-		Gems:         mapToSlice(db.Gems),
-		Encounters:   db.Encounters,
-		Zones:        mapToSlice(db.Zones),
-		Npcs:         mapToSlice(db.Npcs),
-		ItemIcons:    mapToSlice(db.ItemIcons),
-		SpellIcons:   mapToSlice(db.SpellIcons),
-		GlyphIds:     db.GlyphIDs,
-		ReforgeStats: mapToSlice(db.ReforgeStats),
+		Items:          mapToSlice(db.Items),
+		RandomSuffixes: mapToSlice(db.RandomSuffixes),
+		Enchants:       enchants,
+		Gems:           mapToSlice(db.Gems),
+		Encounters:     db.Encounters,
+		Zones:          mapToSlice(db.Zones),
+		Npcs:           mapToSlice(db.Npcs),
+		ItemIcons:      mapToSlice(db.ItemIcons),
+		SpellIcons:     mapToSlice(db.SpellIcons),
+		GlyphIds:       db.GlyphIDs,
+		ReforgeStats:   mapToSlice(db.ReforgeStats),
 	}
 }
 
@@ -239,14 +244,15 @@ func ReadDatabaseFromJson(jsonStr string) *WowDatabase {
 	}
 
 	return &WowDatabase{
-		Items:        sliceToMap(dbProto.Items),
-		Enchants:     enchants,
-		Gems:         sliceToMap(dbProto.Gems),
-		Zones:        sliceToMap(dbProto.Zones),
-		Npcs:         sliceToMap(dbProto.Npcs),
-		ItemIcons:    sliceToMap(dbProto.ItemIcons),
-		SpellIcons:   sliceToMap(dbProto.SpellIcons),
-		ReforgeStats: sliceToMap(dbProto.ReforgeStats),
+		Items:          sliceToMap(dbProto.Items),
+		RandomSuffixes: sliceToMap(dbProto.RandomSuffixes),
+		Enchants:       enchants,
+		Gems:           sliceToMap(dbProto.Gems),
+		Zones:          sliceToMap(dbProto.Zones),
+		Npcs:           sliceToMap(dbProto.Npcs),
+		ItemIcons:      sliceToMap(dbProto.ItemIcons),
+		SpellIcons:     sliceToMap(dbProto.SpellIcons),
+		ReforgeStats:   sliceToMap(dbProto.ReforgeStats),
 	}
 }
 
@@ -276,6 +282,8 @@ func (db *WowDatabase) WriteJson(jsonFilePath string) {
 	buffer.WriteString("{\n")
 
 	tools.WriteProtoArrayToBuffer(uidb.Items, buffer, "items")
+	buffer.WriteString(",\n")
+	tools.WriteProtoArrayToBuffer(uidb.RandomSuffixes, buffer, "randomSuffixes")
 	buffer.WriteString(",\n")
 	tools.WriteProtoArrayToBuffer(uidb.Enchants, buffer, "enchants")
 	buffer.WriteString(",\n")

tools/database/wowhead_db.go
@@ -49,15 +49,98 @@ func ParseWowheadDB(dbContents string) WowheadDatabase {
 				log.Fatalf("failed to parse wowhead item db to json %s\n\n%s", err, dbContents[0:30])
 			}
 		}
+
+		if dbName == "wow.gearPlanner.cata.randomEnchant" {
+			standardized, err := hujson.Standardize([]byte(dbContents)) // Removes invalid JSON, such as trailing commas
+			if err != nil {
+				log.Fatalf("Failed to standardize json %s\n\n%s\n\n%s", err, dbContents[0:30], dbContents[len(dbContents)-30:])
+			}
+
+			err = json.Unmarshal(standardized, &wowheadDB.RandomSuffixes)
+			if err != nil {
+				log.Fatalf("failed to parse wowhead random suffix db to json %s\n\n%s", err, dbContents[0:30])
+			}
+		}
 	}
 
 	fmt.Printf("\n--\nWowhead DB items loaded: %d\n--\n", len(wowheadDB.Items))
+	fmt.Printf("\n--\nWowhead DB random suffixes loaded: %d\n--\n", len(wowheadDB.RandomSuffixes))
 
 	return wowheadDB
 }
 
 type WowheadDatabase struct {
-	Items map[string]WowheadItem
+	Items          map[string]WowheadItem
+	RandomSuffixes map[string]WowheadRandomSuffix
+}
+
+type WowheadRandomSuffix struct {
+	ID    int32                    `json:"id"`
+	Name  string                   `json:"name"`
+	Stats WowheadRandomSuffixStats `json:"stats"`
+}
+
+type WowheadRandomSuffixStats struct {
+	Strength          int32 `json:"str"`
+	Agility           int32 `json:"agi"`
+	Stamina           int32 `json:"sta"`
+	Intellect         int32 `json:"int"`
+	Spirit            int32 `json:"spi"`
+	SpellPower        int32 `json:"spldmg"`
+	MP5               int32 `json:"manargn"`
+	HitRating         int32 `json:"hitrtng"`
+	CritRating        int32 `json:"critstrkrtng"`
+	HasteRating       int32 `json:"hastertng"`
+	AttackPower       int32 `json:"mleatkpwr"`
+	Expertise         int32 `json:"exprtng"`
+	Armor             int32 `json:"armor"`
+	RangedAttackPower int32 `json:"rgdatkpwr"`
+	Block             int32 `json:"blockrtng"`
+	Dodge             int32 `json:"dodgertng"`
+	Parry             int32 `json:"parryrtng"`
+	ArcaneResistance  int32 `json:"arcres"`
+	FireResistance    int32 `json:"firres"`
+	FrostResistance   int32 `json:"frores"`
+	NatureResistance  int32 `json:"natres"`
+	ShadowResistance  int32 `json:"shares"`
+	Mastery           int32 `json:"mastrtng"`
+}
+
+func (wrs WowheadRandomSuffix) ToProto() *proto.ItemRandomSuffix {
+	stats := Stats{
+		proto.Stat_StatStrength:          float64(wrs.Stats.Strength),
+		proto.Stat_StatAgility:           float64(wrs.Stats.Agility),
+		proto.Stat_StatStamina:           float64(wrs.Stats.Stamina),
+		proto.Stat_StatIntellect:         float64(wrs.Stats.Intellect),
+		proto.Stat_StatSpirit:            float64(wrs.Stats.Spirit),
+		proto.Stat_StatSpellPower:        float64(wrs.Stats.SpellPower),
+		proto.Stat_StatMP5:               float64(wrs.Stats.MP5),
+		proto.Stat_StatSpellHit:          float64(wrs.Stats.HitRating),
+		proto.Stat_StatSpellCrit:         float64(wrs.Stats.CritRating),
+		proto.Stat_StatSpellHaste:        float64(wrs.Stats.HasteRating),
+		proto.Stat_StatAttackPower:       float64(wrs.Stats.AttackPower),
+		proto.Stat_StatMeleeHit:          float64(wrs.Stats.HitRating),
+		proto.Stat_StatMeleeCrit:         float64(wrs.Stats.CritRating),
+		proto.Stat_StatMeleeHaste:        float64(wrs.Stats.HasteRating),
+		proto.Stat_StatExpertise:         float64(wrs.Stats.Expertise),
+		proto.Stat_StatArmor:             float64(wrs.Stats.Armor),
+		proto.Stat_StatRangedAttackPower: float64(wrs.Stats.RangedAttackPower),
+		proto.Stat_StatBlock:             float64(wrs.Stats.Block),
+		proto.Stat_StatDodge:             float64(wrs.Stats.Dodge),
+		proto.Stat_StatParry:             float64(wrs.Stats.Parry),
+		proto.Stat_StatArcaneResistance:  float64(wrs.Stats.ArcaneResistance),
+		proto.Stat_StatFireResistance:    float64(wrs.Stats.FireResistance),
+		proto.Stat_StatFrostResistance:   float64(wrs.Stats.FrostResistance),
+		proto.Stat_StatNatureResistance:  float64(wrs.Stats.NatureResistance),
+		proto.Stat_StatShadowResistance:  float64(wrs.Stats.ShadowResistance),
+		proto.Stat_StatMastery:           float64(wrs.Stats.Mastery),
+	}
+
+	return &proto.ItemRandomSuffix{
+		Id:    wrs.ID,
+		Name:  wrs.Name,
+		Stats: toSlice(stats),
+	}
 }
 
 type WowheadItem struct {