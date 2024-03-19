a6a2bc381fbb754cde3ad6a157685df0be0319fd
ToxicKevinFerm <kevin.ferm@toxic.se>
2024-03-20 00:53:07 +0100

    Remove unused function

--- sim/hunter/cata_items.go
+++ sim/hunter/cata_items.go
@@ -4,10 +4,6 @@ import (
 	"github.com/wowsims/cata/sim/core"
 )
 
-func (hunter *Hunter) lightningChargedBattleGearCritBonus() float64 {
-	return core.TernaryFloat64(hunter.HasSetBonus(ItemSetLightningChargedBattleGear, 2), 5, 0)
-}
-
 var ItemSetLightningChargedBattleGear = core.NewItemSet(core.ItemSet{
 	Name: "Lightning-Charged Battlegear",
 	Bonuses: map[int32]core.ApplyEffect{
