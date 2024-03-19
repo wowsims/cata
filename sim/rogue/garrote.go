3055208ffc4eecb6648d570d7828445cdcbc5a92
TheBackstabi <thebackstabi@gmail.com>
2024-03-18 23:37:58 -0400

    Crit Multiplier because I don't test things

--- sim/rogue/garrote.go
+++ sim/rogue/garrote.go
@@ -31,6 +31,7 @@ func (rogue *Rogue) registerGarrote() {
 
 		DamageMultiplier: 1 +
 			0.10*float64(rogue.Talents.Opportunity),
+		CritMultiplier:   1,
 		ThreatMultiplier: 1,
 
 		Dot: core.DotConfig{
