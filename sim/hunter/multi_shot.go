b4c1d3db6d6767ab481e7f18f3d4e8695d94504f
ToxicKevinFerm <kevin.ferm@toxic.se>
2024-03-23 15:03:53 +0100

    Calc damage on apply

--- sim/hunter/multi_shot.go
+++ sim/hunter/multi_shot.go
@@ -35,11 +35,17 @@ func (hunter *Hunter) registerMultiShotSpell() {
 			sharedDmg := hunter.AutoAttacks.Ranged().BaseDamage(sim) +
 				spell.BonusWeaponDamage() //
 
+			baseDamageArray := make([]float64, numHits)
+			for hitIndex := int32(0); hitIndex < numHits; hitIndex++ {
+				currentTarget := hunter.Env.GetTargetUnit(hitIndex)
+				baseDamage := sharedDmg + 0.2*spell.RangedAttackPower(currentTarget)
+				baseDamageArray[hitIndex] = baseDamage
+
+			}
 			spell.WaitTravelTime(sim, func(sim *core.Simulation) {
 				curTarget := target
 				for hitIndex := int32(0); hitIndex < numHits; hitIndex++ {
-					baseDamage := sharedDmg + 0.2*spell.RangedAttackPower(curTarget)
-					spell.CalcAndDealDamage(sim, curTarget, baseDamage, spell.OutcomeRangedHitAndCrit)
+					spell.CalcAndDealDamage(sim, curTarget, baseDamageArray[hitIndex], spell.OutcomeRangedHitAndCrit)
 
 					curTarget = sim.Environment.NextTargetUnit(curTarget)
 				}
