b74d960f0aa1fd319c4370be8980e0bf4a0dbed3
Marcel Ellermann <marcel.ellermann@hotmail.de>
2024-04-16 13:26:41 +0200

    [Trinkets] Fixup Harmlight Token multi target selection

--- sim/common/cata/other_effects.go
+++ sim/common/cata/other_effects.go
@@ -88,7 +88,7 @@ func init() {
 					dot.CalcAndDealPeriodicSnapshotDamage(sim, target, dot.OutcomeSnapshotCrit)
 					if sim.Proc(0.1, "Vengeful Wisp") {
 						// select random proc target
-						spreadTarget := sim.Encounter.TargetUnits[int(sim.Roll(0, float64(len(sim.Encounter.TargetUnits))-1))]
+						spreadTarget := sim.Encounter.TargetUnits[int(sim.Roll(0, float64(len(sim.Encounter.TargetUnits))))]
 
 						// refresh dot on next step - refreshing potentially on aura expire
 						// which will cause nasty things to happen
@@ -129,7 +129,7 @@ func init() {
 
 					if sim.Proc(0.1, "Vengeful Wisp") {
 						// select random proc target
-						spreadTarget := sim.Encounter.TargetUnits[int(sim.Roll(0, float64(len(sim.Encounter.TargetUnits))-1))]
+						spreadTarget := sim.Encounter.TargetUnits[int(sim.Roll(0, float64(len(sim.Encounter.TargetUnits))))]
 						spreadDot.Dot(spreadTarget).Apply(sim) // refresh self on
 					}
 				},
