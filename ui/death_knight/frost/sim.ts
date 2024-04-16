f36ce340581f160fc92b9af40679a0544701bad0
Rosen Rusinov <rosen.rusinov@scopely.com>
2024-04-17 01:30:59 +0200

    add frost/unholy presets and tests and move them to alpha

--- ui/death_knight/frost/sim.ts
+++ ui/death_knight/frost/sim.ts
@@ -50,7 +50,7 @@ const SPEC_CONFIG = registerSpecConfig(Spec.SpecFrostDeathKnight, {
 	],
 	defaults: {
 		// Default equipped gear.
-		gear: Presets.DEFAULT_GEAR_PRESET.gear,
+		gear: Presets.P1_GEAR_PRESET.gear,
 		// Default EP weights for sorting gear in the gear picker.
 		epWeights: Stats.fromMap(
 			{
@@ -78,33 +78,26 @@ const SPEC_CONFIG = registerSpecConfig(Spec.SpecFrostDeathKnight, {
 		specOptions: Presets.DefaultOptions,
 		// Default raid/party buffs settings.
 		raidBuffs: RaidBuffs.create({
+			devotionAura: true,
+			bloodlust: true,
 			markOfTheWild: true,
-			communion: true,
 			icyTalons: true,
 			leaderOfThePack: true,
-			bloodlust: true,
-			hornOfWinter: true,
-			stoneskinTotem: true,
-			moonkinForm: true,
-			wrathOfAirTotem: true,
 			powerWordFortitude: true,
-			arcaneBrilliance: true,
-			blessingOfKings: true,
+			strengthOfEarthTotem: true,
+			abominationsMight: true,
 			blessingOfMight: true,
+			arcaneTactics: true,
 		}),
 		partyBuffs: PartyBuffs.create({
 			heroicPresence: false,
 		}),
 		individualBuffs: IndividualBuffs.create({}),
 		debuffs: Debuffs.create({
-			bloodFrenzy: true,
 			sunderArmor: true,
+			brittleBones: true,
 			ebonPlaguebringer: true,
-			mangle: true,
-			criticalMass: true,
-			demoralizingShout: true,
-			frostFever: true,
-			judgement: true,
+			shadowAndFlame: true
 		}),
 	},
 
@@ -121,8 +114,14 @@ const SPEC_CONFIG = registerSpecConfig(Spec.SpecFrostDeathKnight, {
 	playerIconInputs: [],
 	petConsumeInputs: [],
 	// Buff and Debuff inputs to include/exclude, overriding the EP-based defaults.
-	includeBuffDebuffInputs: [BuffDebuffInputs.SpellDamageDebuff, BuffDebuffInputs.StaminaBuff],
-	excludeBuffDebuffInputs: [BuffDebuffInputs.DamageReduction, BuffDebuffInputs.MeleeAttackSpeedDebuff],
+	includeBuffDebuffInputs: [
+		BuffDebuffInputs.SpellDamageDebuff
+	],
+	excludeBuffDebuffInputs: [
+		BuffDebuffInputs.DamageReduction,
+		BuffDebuffInputs.MeleeAttackSpeedDebuff,
+		BuffDebuffInputs.BleedDebuff
+	],
 	// Inputs to include in the 'Other' section on the settings tab.
 	otherInputs: {
 		inputs: [
@@ -137,17 +136,21 @@ const SPEC_CONFIG = registerSpecConfig(Spec.SpecFrostDeathKnight, {
 	},
 	itemSwapSlots: [ItemSlot.ItemSlotMainHand, ItemSlot.ItemSlotOffHand],
 	encounterPicker: {
-		// Whether to include 'Execute Duration (%)' in the 'Encounter' section of the settings tab.
 		showExecuteProportion: false,
 	},
 
 	presets: {
-		// Preset talents that the user can quickly select.
-		talents: [],
-		// Preset rotations that the user can quickly select.
-		rotations: [],
-		// Preset gear configurations that the user can quickly select.
-		gear: [],
+		talents: [
+			Presets.SingleTargetTalents,
+			//Presets.AoeTalents,
+		],
+		rotations: [
+			Presets.SINGLE_TARGET_ROTATION_PRESET_DEFAULT,
+			//Presets.AOE_ROTATION_PRESET_DEFAULT,
+		],
+		gear: [
+			Presets.P1_GEAR_PRESET,
+		],
 	},
 
 	raidSimPresets: [
@@ -164,16 +167,16 @@ const SPEC_CONFIG = registerSpecConfig(Spec.SpecFrostDeathKnight, {
 			defaultGear: {
 				[Faction.Unknown]: {},
 				[Faction.Alliance]: {
-					1: Presets.DEFAULT_GEAR_PRESET.gear,
-					2: Presets.DEFAULT_GEAR_PRESET.gear,
-					3: Presets.DEFAULT_GEAR_PRESET.gear,
-					4: Presets.DEFAULT_GEAR_PRESET.gear,
+					1: Presets.P1_GEAR_PRESET.gear,
+					2: Presets.P1_GEAR_PRESET.gear,
+					3: Presets.P1_GEAR_PRESET.gear,
+					4: Presets.P1_GEAR_PRESET.gear,
 				},
 				[Faction.Horde]: {
-					1: Presets.DEFAULT_GEAR_PRESET.gear,
-					2: Presets.DEFAULT_GEAR_PRESET.gear,
-					3: Presets.DEFAULT_GEAR_PRESET.gear,
-					4: Presets.DEFAULT_GEAR_PRESET.gear,
+					1: Presets.P1_GEAR_PRESET.gear,
+					2: Presets.P1_GEAR_PRESET.gear,
+					3: Presets.P1_GEAR_PRESET.gear,
+					4: Presets.P1_GEAR_PRESET.gear,
 				},
 			},
 			otherDefaults: Presets.OtherDefaults,
