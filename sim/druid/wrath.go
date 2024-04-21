8c3f154843776d4dd695459acaa5b3aac49d0e99
Stayko Nedev <zakkgard@gmail.com>
2024-04-21 13:31:01 +0300

    basic wrath implementation, removing references to item sets and talents that are removed in cataclysm

--- sim/druid/wrath.go
+++ sim/druid/wrath.go
@@ -6,13 +6,8 @@ import (
 	"github.com/wowsims/cata/sim/core"
 )
 
-const IdolAvenger int32 = 31025
-const IdolSteadfastRenewal int32 = 40712
-
 func (druid *Druid) registerWrathSpell() {
-	spellCoeff := 0.571 + (0.02 * float64(druid.Talents.WrathOfCenarius))
-	bonusFlatDamage := core.TernaryFloat64(druid.Ranged().ID == IdolAvenger, 25, 0) +
-		core.TernaryFloat64(druid.Ranged().ID == IdolSteadfastRenewal, 70, 0)
+	spellCoeff := 0.571
 
 	druid.Wrath = druid.RegisterSpell(Humanoid|Moonkin, core.SpellConfig{
 		ActionID:     core.ActionID{SpellID: 48461},
@@ -32,16 +27,13 @@ func (druid *Druid) registerWrathSpell() {
 			},
 		},
 
-		BonusCritRating: 0 +
-			2*float64(druid.Talents.NaturesMajesty)*core.CritRatingPerCritChance +
-			core.TernaryFloat64(druid.HasSetBonus(ItemSetDreamwalkerGarb, 4), 5*core.CritRatingPerCritChance, 0),
-		DamageMultiplier: (1 + []float64{0.0, 0.03, 0.06, 0.1}[druid.Talents.Moonfury]) *
-			core.TernaryFloat64(druid.HasSetBonus(ItemSetMalfurionsRegalia, 4), 1.04, 1),
+		BonusCritRating:  2 * float64(druid.Talents.NaturesMajesty) * core.CritRatingPerCritChance,
+		DamageMultiplier: 1,
 		CritMultiplier:   druid.BalanceCritMultiplier(),
 		ThreatMultiplier: 1,
 
 		ApplyEffects: func(sim *core.Simulation, target *core.Unit, spell *core.Spell) {
-			baseDamage := bonusFlatDamage + sim.Roll(557, 627) + spellCoeff*spell.SpellPower()
+			baseDamage := sim.Roll(557, 627) + spellCoeff*spell.SpellPower()
 			result := spell.CalcDamage(sim, target, baseDamage, spell.OutcomeMagicHitAndCrit)
 			spell.WaitTravelTime(sim, func(sim *core.Simulation) {
 				spell.DealDamage(sim, result)
