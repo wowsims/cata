3bee96837ba6842203f43b12147cfbb8a36c2e09
Stayko Nedev <zakkgard@gmail.com>
2024-04-22 20:23:47 +0300

    more wrath work - spell coeff, mana cost, spellid, base damage, glyph of wrath

--- sim/druid/wrath.go
+++ sim/druid/wrath.go
@@ -4,36 +4,41 @@ import (
 	"time"
 
 	"github.com/wowsims/cata/sim/core"
+	"github.com/wowsims/cata/sim/core/proto"
 )
 
 func (druid *Druid) registerWrathSpell() {
-	spellCoeff := 0.571
+	spellCoeff := 0.879
 
 	druid.Wrath = druid.RegisterSpell(Humanoid|Moonkin, core.SpellConfig{
-		ActionID:     core.ActionID{SpellID: 48461},
+		ActionID:     core.ActionID{SpellID: 5176},
 		SpellSchool:  core.SpellSchoolNature,
 		ProcMask:     core.ProcMaskSpellDamage,
 		Flags:        SpellFlagNaturesGrace | SpellFlagOmenTrigger | core.SpellFlagAPL,
 		MissileSpeed: 20,
 
 		ManaCost: core.ManaCostOptions{
-			BaseCost:   0.11,
+			BaseCost:   0.09,
 			Multiplier: 1 - 0.03*float64(druid.Talents.Moonglow),
 		},
+
 		Cast: core.CastConfig{
 			DefaultCast: core.Cast{
 				GCD:      core.GCDDefault,
-				CastTime: time.Second*2 - time.Millisecond*100*time.Duration(druid.Talents.StarlightWrath),
+				CastTime: time.Millisecond*2500 - time.Millisecond*100*time.Duration(druid.Talents.StarlightWrath),
 			},
 		},
 
-		BonusCritRating:  2 * float64(druid.Talents.NaturesMajesty) * core.CritRatingPerCritChance,
-		DamageMultiplier: 1,
-		CritMultiplier:   druid.BalanceCritMultiplier(),
+		BonusCritRating: 2 * float64(druid.Talents.NaturesMajesty) * core.CritRatingPerCritChance,
+
+		DamageMultiplier: 1 + core.TernaryFloat64(druid.HasPrimeGlyph(proto.DruidPrimeGlyph_GlyphOfWrath), 0.1, 0),
+
+		CritMultiplier: druid.BalanceCritMultiplier(),
+
 		ThreatMultiplier: 1,
 
 		ApplyEffects: func(sim *core.Simulation, target *core.Unit, spell *core.Spell) {
-			baseDamage := sim.Roll(557, 627) + spellCoeff*spell.SpellPower()
+			baseDamage := sim.Roll(675, 761) + spellCoeff*spell.SpellPower()
 			result := spell.CalcDamage(sim, target, baseDamage, spell.OutcomeMagicHitAndCrit)
 			spell.WaitTravelTime(sim, func(sim *core.Simulation) {
 				spell.DealDamage(sim, result)
