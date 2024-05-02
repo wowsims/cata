b060afb492cb8ee312db10f530f3b14a5184d0f0
Adrian Klingen <adrian.klingen@deptagency.com>
2024-05-02 12:51:49 +0200

    Add itemID resolve fix

--- ui/core/components/importers.ts
+++ ui/core/components/importers.ts
@@ -566,8 +566,10 @@ function glyphToID(glyph: string | JsonObject, db: Database, glyphsConfig: Recor
 		// Legacy version: AddOn exports Glyphs by name (string) only. Names must be in English.
 		return glyphNameToID(glyph, glyphsConfig);
 	}
+
 	// Cata version exports glyph information in a table that includes the glyph itemID.
-	if (glyph.itemID) return glyph.itemID as number;
+	// @todo: Need to update glyph_id_map.json in a future revision with all the new Cata spellIDs
+	if (glyph.itemID) return db.glyphSpellToItemId(glyph.itemID as number);
 
 	// Wotlk version exports glyph information in a table that includes the name and the glyph spell ID.
 	return db.glyphSpellToItemId(glyph.spellID as number);
