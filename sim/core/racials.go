21277649ce71d8f323b76de88f1e294eb5dcf075
Rosen Rusinov <rosen.rusinov@scopely.com>
2024-05-09 21:41:03 +0200

    fix dwarf racial expertise bonus

--- sim/core/racials.go
+++ sim/core/racials.go
@@ -82,7 +82,7 @@ func applyRaceEffects(agent Agent) {
 			character.AddBonusRangedCritRating(1 * CritRatingPerCritChance)
 		}
 
-		applyWeaponSpecialization(character, 5*ExpertisePerQuarterPercentReduction,
+		applyWeaponSpecialization(character, 3*ExpertisePerQuarterPercentReduction,
 			proto.WeaponType_WeaponTypeMace)
 
 		actionID := ActionID{SpellID: 20594}
