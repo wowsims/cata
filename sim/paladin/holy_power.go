074b28ccf4613a302da47db364f1ca8b23f5a7f5
Swedge <52763122+hypernormalisation@users.noreply.github.com>
2024-05-15 12:05:53 +0100

    holy power log fix, added .css for holy power log formatting

--- sim/paladin/holy_power.go
+++ sim/paladin/holy_power.go
@@ -42,7 +42,7 @@ func (pb *HolyPowerBar) GainHolyPower(sim *core.Simulation, amountToAdd int32, m
 	metrics.AddEvent(float64(newHolyPower), float64(newHolyPower-pb.holyPower))
 
 	if sim.Log != nil {
-		sim.Log("Gained %d holy power from %s (%d --> %d) of %0.0f total.", newHolyPower, metrics.ActionID, pb.holyPower, newHolyPower, 3.0)
+		pb.paladin.Log(sim, "Gained %d holy power from %s (%d --> %d) of %0.0f total.", newHolyPower, metrics.ActionID, pb.holyPower, newHolyPower, 3.0)
 	}
 
 	pb.holyPower = newHolyPower
@@ -54,7 +54,7 @@ func (pb *HolyPowerBar) SpendHolyPower(sim *core.Simulation, metrics *core.Resou
 	}
 
 	if sim.Log != nil {
-		sim.Log("Spent %d holy power from %s (%d --> %d) of %0.0f total.", pb.holyPower, metrics.ActionID, pb.holyPower, 0, 3.0)
+		pb.paladin.Log(sim, "Spent %d holy power from %s (%d --> %d) of %0.0f total.", pb.holyPower, metrics.ActionID, pb.holyPower, 0, 3.0)
 	}
 
 	metrics.AddEvent(float64(-pb.holyPower), float64(-pb.holyPower))
