44d808fa6b26abe8d21e82440b906a69d79fd591
NerdEgghead <nerdeggheadtbc@gmail.com>
2024-05-17 16:24:04 -0700

    Updated Simple rotation to match optimized APL
     On branch feral
     Changes to be committed:
    	modified:   proto/druid.proto
    	modified:   ui/core/individual_sim_ui.tsx
    	modified:   ui/core/player.ts
    	modified:   ui/core/preset_utils.ts
    	modified:   ui/druid/guardian/inputs.ts
    	modified:   ui/druid/guardian/presets.ts
    	modified:   ui/druid/guardian/sim.ts

--- ui/core/individual_sim_ui.tsx
+++ ui/core/individual_sim_ui.tsx
@@ -27,6 +27,7 @@ import { APLRotation, APLRotation_Type as APLRotationType } from './proto/apl';
 import {
 	Class,
 	Consumes,
+	Cooldowns,
 	Debuffs,
 	Encounter as EncounterProto,
 	EquipmentSpec,
@@ -465,6 +466,12 @@ export abstract class IndividualSimUI<SpecType extends Spec> extends SimUI {
 
 			const defaultSimpleRotation = this.individualConfig.defaults.simpleRotation || this.player.specTypeFunctions.rotationCreate();
 			this.player.setSimpleRotation(eventID, defaultSimpleRotation);
+			this.player.setSimpleCooldowns(
+				eventID,
+				Cooldowns.create({
+					hpPercentForDefensives: this.player.playerSpec.isTankSpec ? 0.4 : 0,
+				}),
+			);
 		});
 	}
 
