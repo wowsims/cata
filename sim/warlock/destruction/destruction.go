a77644c9124c508b571696a70139b504e2845bde
Tharre <tharre3@gmail.com>
2024-05-31 20:13:09 +0200

    warlock: make trinkets&bombs scale with destro mastery
    Even though it has spell masks listed in the spell data, in-game tests
    show that trinkets and bombs that do fire damage still scale with it.

--- sim/warlock/destruction/destruction.go
+++ sim/warlock/destruction/destruction.go
@@ -58,8 +58,8 @@ func (destruction *DestructionWarlock) ApplyTalents() {
 
 	// Mastery: Fiery Apocalypse
 	masteryMod := destruction.AddDynamicMod(core.SpellModConfig{
-		Kind:      core.SpellMod_DamageDone_Pct,
-		ClassMask: warlock.WarlockFireDamage,
+		Kind:   core.SpellMod_DamageDone_Pct,
+		School: core.SpellSchoolFire,
 	})
 
 	destruction.AddOnMasteryStatChanged(func(sim *core.Simulation, oldMastery float64, newMastery float64) {
