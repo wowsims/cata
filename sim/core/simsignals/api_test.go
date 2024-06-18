cc3262726ba7dcaa27f972b9a05c07e63a2e5b1b
FelixPflaum <141590183+FelixPflaum@users.noreply.github.com>
2024-06-18 18:04:17 +0200

    Add abort test case for weights and bulk sim

--- sim/core/simsignals/api_test.go
+++ sim/core/simsignals/api_test.go
@@ -10,35 +10,35 @@ import (
 	"github.com/wowsims/cata/sim/warrior/arms"
 )
 
-func getTestRsr() *proto.RaidSimRequest {
+func TestAbort(t *testing.T) {
 	arms.RegisterArmsWarrior()
-	return &proto.RaidSimRequest{
-		RequestId: "uniqueidlol",
-		Raid: core.SinglePlayerRaidProto(
-			&proto.Player{
-				Race:      proto.Race_RaceOrc,
-				Class:     proto.Class_ClassWarrior,
-				Equipment: &proto.EquipmentSpec{},
-				Rotation:  &proto.APLRotation{},
-				Consumes:  &proto.Consumes{},
-				Spec: &proto.Player_ArmsWarrior{
-					ArmsWarrior: &proto.ArmsWarrior{
-						Options: &proto.ArmsWarrior_Options{
-							ClassOptions: &proto.WarriorOptions{
-								StartingRage:       50,
-								UseShatteringThrow: true,
-								Shout:              proto.WarriorShout_WarriorShoutBattle,
-							},
-						},
+
+	player := &proto.Player{
+		Name:      "John",
+		Race:      proto.Race_RaceOrc,
+		Class:     proto.Class_ClassWarrior,
+		Equipment: core.GetGearSet("../../../ui/warrior/arms/gear_sets", "p1_arms_bis").GearSet,
+		Rotation:  &proto.APLRotation{},
+		Consumes:  &proto.Consumes{},
+		Spec: &proto.Player_ArmsWarrior{
+			ArmsWarrior: &proto.ArmsWarrior{
+				Options: &proto.ArmsWarrior_Options{
+					ClassOptions: &proto.WarriorOptions{
+						StartingRage:       50,
+						UseShatteringThrow: true,
+						Shout:              proto.WarriorShout_WarriorShoutBattle,
 					},
 				},
-				Glyphs:        &proto.Glyphs{},
-				TalentsString: "",
-				Buffs:         &proto.IndividualBuffs{},
 			},
-			core.FullPartyBuffs,
-			core.FullRaidBuffs,
-			core.FullDebuffs),
+		},
+		Glyphs:        &proto.Glyphs{},
+		TalentsString: "",
+		Buffs:         &proto.IndividualBuffs{},
+	}
+
+	rsr := &proto.RaidSimRequest{
+		RequestId: "uniqueidlol",
+		Raid:      core.SinglePlayerRaidProto(player, core.FullPartyBuffs, core.FullRaidBuffs, core.FullDebuffs),
 		Encounter: &proto.Encounter{
 			Duration: 300,
 			Targets: []*proto.Target{
@@ -51,10 +51,6 @@ func getTestRsr() *proto.RaidSimRequest {
 			RandomSeed: 123,
 		},
 	}
-}
-
-func TestAbort(t *testing.T) {
-	rsr := getTestRsr()
 
 	t.Run("RunRaidSimAsync", func(t *testing.T) {
 		progress := make(chan *proto.ProgressMetrics, 10)
@@ -137,10 +133,10 @@ func TestAbort(t *testing.T) {
 		}
 	})
 
-	/* t.Run("StatWeightsAsync", func(t *testing.T) {
+	t.Run("StatWeightsAsync", func(t *testing.T) {
 		swr := &proto.StatWeightsRequest{
-			Id:         "lel",
-			Player:     getTestPlayerFeralCat(),
+			RequestId:  "lel",
+			Player:     player,
 			RaidBuffs:  core.FullRaidBuffs,
 			PartyBuffs: core.FullPartyBuffs,
 			Debuffs:    core.FullDebuffs,
@@ -164,16 +160,46 @@ func TestAbort(t *testing.T) {
 
 		go func() {
 			time.Sleep(time.Second)
-			core.AbortSimById(swr.Id)
+			simsignals.AbortById(swr.RequestId)
 		}()
 
 		for msg := range progress {
 			if msg.FinalWeightResult != nil {
-				if msg.FinalWeightResult.ErrorResult != "aborted" || msg.FinalRaidResult.Outcome != proto.SimOutcomeType_SimOutcomeAborted {
-					t.Fatalf("Sim did not abort! %s", msg.FinalWeightResult.ErrorResult)
+				if msg.FinalWeightResult.Error == nil || msg.FinalWeightResult.Error.Type != proto.ErrorOutcomeType_ErrorOutcomeAborted {
+					t.Fatalf("Sim did not abort!")
 				}
 				return
 			}
 		}
-	}) */
+	})
+
+	t.Run("RunBulkSimAsync", func(t *testing.T) {
+		bsr := &proto.BulkSimRequest{
+			RequestId:    "lel",
+			BaseSettings: rsr,
+			BulkSettings: &proto.BulkSettings{
+				Combinations:       true,
+				Items:              []*proto.ItemSpec{{Id: 77949}, {Id: 55068}},
+				IterationsPerCombo: 9999,
+				FastMode:           false,
+			},
+		}
+
+		progress := make(chan *proto.ProgressMetrics, 10)
+		core.RunBulkSimAsync(bsr, progress)
+
+		go func() {
+			time.Sleep(time.Second)
+			simsignals.AbortById(bsr.RequestId)
+		}()
+
+		for msg := range progress {
+			if msg.FinalBulkResult != nil {
+				if msg.FinalBulkResult.Error == nil || msg.FinalBulkResult.Error.Type != proto.ErrorOutcomeType_ErrorOutcomeAborted {
+					t.Fatalf("Sim did not abort!")
+				}
+				return
+			}
+		}
+	})
 }
