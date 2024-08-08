5bddca030e651371776952b6adb8e13bb2aade74
Adrian Klingen <adrian.klingen@deptagency.com>
2024-08-08 18:42:54 +0200

    Round up rating values

--- ui/warlock/demonology/sim.ts
+++ ui/warlock/demonology/sim.ts
@@ -56,7 +56,7 @@ const SPEC_CONFIG = registerSpecConfig(Spec.SpecDemonologyWarlock, {
 			const initialMasteryOffset = Math.round((Mechanics.MASTERY_RATING_PER_MASTERY_POINT / masteryPercentPerPoint) * 0.6);
 			for (let masteryPercent = 0; masteryPercent <= 200; masteryPercent++) {
 				masteryRatingBreakpoints.push(
-					Math.round(initialMasteryOffset + masteryPercent * (Mechanics.MASTERY_RATING_PER_MASTERY_POINT / masteryPercentPerPoint)),
+					Math.ceil(initialMasteryOffset + masteryPercent * (Mechanics.MASTERY_RATING_PER_MASTERY_POINT / masteryPercentPerPoint)),
 				);
 			}
 
