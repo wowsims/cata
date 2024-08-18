2fffcfe3eb4e1c21992904dc30566b7c083191cd
Marcel Ellermann <marcel.ellermann@hotmail.de>
2024-08-18 12:41:46 +0200

    [Metrics] Add correct cast time metrics for channels

--- sim/core/dot.go
+++ sim/core/dot.go
@@ -252,12 +252,16 @@ func newDot(config Dot) *Dot {
 				dot.Spell.Unit.WaitUntil(sim, sim.CurrentTime+dot.Spell.Unit.ChannelClipDelay)
 			}
 		}
+
 		dot.tickAction.Cancel(sim)
 		dot.tickAction = nil
 		if dot.isChanneled {
 			dot.Spell.Unit.ChanneledDot = nil
 			dot.Spell.Unit.Rotation.interruptChannelIf = nil
 			dot.Spell.Unit.Rotation.allowChannelRecastOnInterrupt = false
+
+			// track time metrics for channels
+			dot.Spell.SpellMetrics[aura.Unit.UnitIndex].TotalCastTime += dot.TimeActive(sim)
 		}
 	})
 
