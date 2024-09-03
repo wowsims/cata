9ddfb81a9f804db3345441df52e0113d4ac2b8f3
Adrian Klingen <adrian.klingen@deptagency.com>
2024-09-03 21:48:28 +0200

    Change notice to all specs

--- ui/core/constants/item_notices.tsx
+++ ui/core/constants/item_notices.tsx
@@ -1,15 +1,11 @@
 import { ItemNoticeData } from '../components/item_notice/item_notice';
 import { Spec } from '../proto/common';
 
-const DTR_PARTIAL_IMPLEMENTATION_WARNING = (
+const DTR_MISSING_IMPLEMENTATION_WARNING = (
 	<>
 		<p className="fw-bold">This item is not implemented!</p>
-		<p>
-			We are working hard on gathering all the old resources to allow for an initial implementation.
-		</p>
-		<p className="mb-0">
-			Want to help out by providing additional information? Contact us on our Discord!
-		</p>
+		<p>We are working hard on gathering all the old resources to allow for an initial implementation.</p>
+		<p className="mb-0">Want to help out by providing additional information? Contact us on our Discord!</p>
 	</>
 );
 
@@ -18,20 +14,16 @@ export const ITEM_NOTICES = new Map<number, ItemNoticeData>([
 	[
 		71086,
 		{
-			[Spec.SpecUnknown]: (
-				<>
-					<p>This item is unsupported for this spec.</p>
-				</>
-			),
-			[Spec.SpecBalanceDruid]: DTR_PARTIAL_IMPLEMENTATION_WARNING,
-			[Spec.SpecArcaneMage]: DTR_PARTIAL_IMPLEMENTATION_WARNING,
-			[Spec.SpecFireMage]: DTR_PARTIAL_IMPLEMENTATION_WARNING,
-			[Spec.SpecFrostMage]: DTR_PARTIAL_IMPLEMENTATION_WARNING,
-			[Spec.SpecShadowPriest]: DTR_PARTIAL_IMPLEMENTATION_WARNING,
-			[Spec.SpecElementalShaman]: DTR_PARTIAL_IMPLEMENTATION_WARNING,
-			[Spec.SpecAfflictionWarlock]: DTR_PARTIAL_IMPLEMENTATION_WARNING,
-			[Spec.SpecDemonologyWarlock]: DTR_PARTIAL_IMPLEMENTATION_WARNING,
-			[Spec.SpecDestructionWarlock]: DTR_PARTIAL_IMPLEMENTATION_WARNING,
+			[Spec.SpecUnknown]: DTR_MISSING_IMPLEMENTATION_WARNING,
+			[Spec.SpecBalanceDruid]: DTR_MISSING_IMPLEMENTATION_WARNING,
+			[Spec.SpecArcaneMage]: DTR_MISSING_IMPLEMENTATION_WARNING,
+			[Spec.SpecFireMage]: DTR_MISSING_IMPLEMENTATION_WARNING,
+			[Spec.SpecFrostMage]: DTR_MISSING_IMPLEMENTATION_WARNING,
+			[Spec.SpecShadowPriest]: DTR_MISSING_IMPLEMENTATION_WARNING,
+			[Spec.SpecElementalShaman]: DTR_MISSING_IMPLEMENTATION_WARNING,
+			[Spec.SpecAfflictionWarlock]: DTR_MISSING_IMPLEMENTATION_WARNING,
+			[Spec.SpecDemonologyWarlock]: DTR_MISSING_IMPLEMENTATION_WARNING,
+			[Spec.SpecDestructionWarlock]: DTR_MISSING_IMPLEMENTATION_WARNING,
 		},
 	],
 ]);
