202614b60600605844381fe947f31009118dbfd8
Johan Hillerström <progr@mmer.nu>
2024-10-03 00:43:50 +0200

    [Ret] Remove test options to not have the ret tests take forever

--- sim/paladin/retribution/retribution_test.go
+++ sim/paladin/retribution/retribution_test.go
@@ -18,35 +18,12 @@ func TestRetribution(t *testing.T) {
 		Race:       proto.Race_RaceBloodElf,
 		OtherRaces: []proto.Race{proto.Race_RaceHuman, proto.Race_RaceDraenei, proto.Race_RaceDwarf, proto.Race_RaceTauren},
 
-		GearSet: core.GetGearSet("../../../ui/paladin/retribution/gear_sets", "t11_bis"),
-		OtherGearSets: []core.GearSetCombo{
-			core.GetGearSet("../../../ui/paladin/retribution/gear_sets", "preraid"),
-			core.GetGearSet("../../../ui/paladin/retribution/gear_sets", "t12_bis"),
-			//core.GetGearSet("../../../ui/paladin/retribution/gear_sets", "t13_bis"),
-		},
-		Talents: StandardTalents,
-		Glyphs:  StandardGlyphs,
-		OtherTalentSets: []core.TalentsCombo{
-			{
-				Label:   "Crusader Strike Glyph",
-				Talents: StandardTalents,
-				Glyphs:  GlyphsWithCrusaderStrike,
-			},
-		},
+		GearSet:     core.GetGearSet("../../../ui/paladin/retribution/gear_sets", "t11_bis"),
+		Talents:     StandardTalents,
+		Glyphs:      StandardGlyphs,
 		Consumes:    FullConsumes,
 		SpecOptions: core.SpecOptionsCombo{Label: "Basic", SpecOptions: DefaultOptions},
-		OtherSpecOptions: []core.SpecOptionsCombo{
-			{Label: "Snapshot Guardian", SpecOptions: OptionsWithSnapshotGuardian},
-			{Label: "Seal of Righteousness", SpecOptions: OptionsWithSealOfRighteousness},
-			{Label: "Seal of Justice", SpecOptions: OptionsWithSealOfJustice},
-			{Label: "Seal of Insight", SpecOptions: OptionsWithSealOfInsight},
-		},
-		Rotation: core.GetAplRotation("../../../ui/paladin/retribution/apls", "default"),
-		OtherRotations: []core.RotationCombo{
-			core.GetAplRotation("../../../ui/paladin/retribution/apls", "apparatus"),
-			//core.GetAplRotation("../../../ui/paladin/retribution/apls", "t13"),
-			//core.GetAplRotation("../../../ui/paladin/retribution/apls", "t13-apparatus"),
-		},
+		Rotation:    core.GetAplRotation("../../../ui/paladin/retribution/apls", "default"),
 
 		ItemFilter: core.ItemFilter{
 			WeaponTypes: []proto.WeaponType{
@@ -108,71 +85,12 @@ var StandardGlyphs = &proto.Glyphs{
 	Minor2: int32(proto.PaladinMinorGlyph_GlyphOfTruth),
 	Minor3: int32(proto.PaladinMinorGlyph_GlyphOfRighteousness),
 }
-var GlyphsWithCrusaderStrike = &proto.Glyphs{
-	Prime1: int32(proto.PaladinPrimeGlyph_GlyphOfTemplarSVerdict),
-	Prime2: int32(proto.PaladinPrimeGlyph_GlyphOfSealOfTruth),
-	Prime3: int32(proto.PaladinPrimeGlyph_GlyphOfCrusaderStrike),
-	Major1: int32(proto.PaladinMajorGlyph_GlyphOfHammerOfWrath),
-	Major2: int32(proto.PaladinMajorGlyph_GlyphOfTheAsceticCrusader),
-	Major3: int32(proto.PaladinMajorGlyph_GlyphOfConsecration),
-	Minor1: int32(proto.PaladinMinorGlyph_GlyphOfBlessingOfMight),
-	Minor2: int32(proto.PaladinMinorGlyph_GlyphOfTruth),
-	Minor3: int32(proto.PaladinMinorGlyph_GlyphOfRighteousness),
-}
 
 var DefaultOptions = &proto.Player_RetributionPaladin{
 	RetributionPaladin: &proto.RetributionPaladin{
 		Options: &proto.RetributionPaladin_Options{
 			ClassOptions: &proto.PaladinOptions{
 				Seal:             proto.PaladinSeal_Truth,
-				Aura:             proto.PaladinAura_Retribution,
-				SnapshotGuardian: false,
-			},
-		},
-	},
-}
-
-var OptionsWithSnapshotGuardian = &proto.Player_RetributionPaladin{
-	RetributionPaladin: &proto.RetributionPaladin{
-		Options: &proto.RetributionPaladin_Options{
-			ClassOptions: &proto.PaladinOptions{
-				Seal:             proto.PaladinSeal_Truth,
-				Aura:             proto.PaladinAura_Retribution,
-				SnapshotGuardian: true,
-			},
-		},
-	},
-}
-
-var OptionsWithSealOfRighteousness = &proto.Player_RetributionPaladin{
-	RetributionPaladin: &proto.RetributionPaladin{
-		Options: &proto.RetributionPaladin_Options{
-			ClassOptions: &proto.PaladinOptions{
-				Seal:             proto.PaladinSeal_Righteousness,
-				Aura:             proto.PaladinAura_Retribution,
-				SnapshotGuardian: false,
-			},
-		},
-	},
-}
-
-var OptionsWithSealOfJustice = &proto.Player_RetributionPaladin{
-	RetributionPaladin: &proto.RetributionPaladin{
-		Options: &proto.RetributionPaladin_Options{
-			ClassOptions: &proto.PaladinOptions{
-				Seal:             proto.PaladinSeal_Justice,
-				Aura:             proto.PaladinAura_Retribution,
-				SnapshotGuardian: false,
-			},
-		},
-	},
-}
-
-var OptionsWithSealOfInsight = &proto.Player_RetributionPaladin{
-	RetributionPaladin: &proto.RetributionPaladin{
-		Options: &proto.RetributionPaladin_Options{
-			ClassOptions: &proto.PaladinOptions{
-				Seal:             proto.PaladinSeal_Insight,
 				Aura:             proto.PaladinAura_Retribution,
 				SnapshotGuardian: false,
 			},
