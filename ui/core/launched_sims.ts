4658203ae6a713225a4561c6f2a231a11f17530a
Johan Hillerström <progr@mmer.nu>
2024-10-31 23:16:50 +0100

    [Frost] Move Frost Death Knight to phase 3

--- ui/core/launched_sims.ts
+++ ui/core/launched_sims.ts
@@ -34,7 +34,7 @@ export const simLaunchStatuses: Record<Spec, SimStatus> = {
 		status: LaunchStatus.Launched,
 	},
 	[Spec.SpecFrostDeathKnight]: {
-		phase: Phase.Phase2,
+		phase: Phase.Phase3,
 		status: LaunchStatus.Launched,
 	},
 	[Spec.SpecUnholyDeathKnight]: {
