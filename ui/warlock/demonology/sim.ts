f08bc63694ec8457cbd46bd753a5f851086580d8
Adrian Klingen <adrian.klingen@deptagency.com>
2025-01-12 11:59:10 +0100

    Add Warlock P4 WIP set

--- ui/warlock/demonology/sim.ts
+++ ui/warlock/demonology/sim.ts
@@ -127,7 +127,7 @@ const SPEC_CONFIG = registerSpecConfig(Spec.SpecDemonologyWarlock, {
 		rotations: [Presets.APL_ShadowBolt, Presets.APL_Incinerate],
 
 		// Preset gear configurations that the user can quickly select.
-		gear: [Presets.PRERAID_PRESET, Presets.P1_PRESET, Presets.P3_PRESET],
+		gear: [Presets.PRERAID_PRESET, Presets.P1_PRESET, Presets.P3_PRESET, Presets.P4_PRESET],
 
 		builds: [Presets.PRESET_BUILD_SHADOWBOLT, Presets.PRESET_BUILD_INCINERATE],
 	},
