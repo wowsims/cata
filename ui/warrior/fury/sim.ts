377cd4083da3801a3e48b2f03a9c5ec303a0dbee
Adrian Klingen <adrian.klingen@deptagency.com>
2025-02-04 10:37:20 +0100

    Add P4 Fury WIP sets

--- ui/warrior/fury/sim.ts
+++ ui/warrior/fury/sim.ts
@@ -162,9 +162,11 @@ const SPEC_CONFIG = registerSpecConfig(Spec.SpecFuryWarrior, {
 			Presets.P1_BIS_FURY_TG_PRESET,
 			Presets.P3_BIS_FURY_SMF_PRESET,
 			Presets.P3_BIS_FURY_TG_PRESET,
+			Presets.P4_BIS_FURY_TG_PRESET,
+			Presets.P4_BIS_FURY_SMF_PRESET,
 		],
-		itemSwaps: [Presets.P3_ITEM_SWAP_SMF, Presets.P3_ITEM_SWAP_TG],
-		builds: [Presets.P1_PRESET_BUILD_SMF, Presets.P1_PRESET_BUILD_TG, Presets.P3_PRESET_BUILD_SMF, Presets.P3_PRESET_BUILD_TG],
+		itemSwaps: [Presets.P3_ITEM_SWAP_SMF, Presets.P3_ITEM_SWAP_TG, Presets.P4_ITEM_SWAP_TG,Presets.P4_ITEM_SWAP_SMF],
+		builds: [Presets.P1_PRESET_BUILD_SMF, Presets.P1_PRESET_BUILD_TG, Presets.P3_PRESET_BUILD_SMF, Presets.P3_PRESET_BUILD_TG, Presets.P4_PRESET_BUILD_TG, Presets.P4_PRESET_BUILD_SMF],
 	},
 
 	autoRotation: (_player: Player<Spec.SpecFuryWarrior>): APLRotation => {
