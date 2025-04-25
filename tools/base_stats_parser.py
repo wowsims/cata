315467cb907e997456894d408d9b08cca27fb071
Casey Raethke <casey.j.raethke@gmail.com>
2025-04-25 12:57:50 -0500

    Fix imports

--- tools/base_stats_parser.py
+++ tools/base_stats_parser.py
@@ -73,8 +73,8 @@ package core
 // **************************************
 
 import (
-	"github.com/wowsims/cata/sim/core/proto"
-	"github.com/wowsims/cata/sim/core/stats"
+	"github.com/wowsims/mop/sim/core/proto"
+	"github.com/wowsims/mop/sim/core/stats"
 )
 
 '''
