2c0f53af7d2f6400f2533cdbb02dc0f293552f5f
Justin W <justinwilkowski@gmail.com>
2025-05-20 15:48:21 -0700

    Frozen Orb Implementation, based on flame orb but slightly different.

---
+++ sim/mage/frost/_frozen_orb.go
@@ -0,0 +1,151 @@
+package frost
+
+import (
+	"time"
+
+	"github.com/wowsims/mop/sim/core"
+	"github.com/wowsims/mop/sim/core/stats"
+	"github.com/wowsims/mop/sim/mage"
+)
+
+func (frost *FrostMage) registerFrozenOrbSpell() {
+
+	frozenOrb := mage.RegisterSpell(core.SpellConfig{
+		ActionID:       core.ActionID{SpellID: 84714},
+		SpellSchool:    core.SpellSchoolFrost,
+		ProcMask:       core.ProcMaskSpellDamage,
+		Flags:          core.SpellFlagAPL,
+		ClassSpellMask: MageSpellFrozenOrb,
+
+		ManaCost: core.ManaCostOptions{
+			BaseCostPercent: 6,
+		},
+		Cast: core.CastConfig{
+			DefaultCast: core.Cast{
+				GCD: core.GCDDefault,
+			},
+			CD: core.Cooldown{
+				Timer:    mage.NewTimer(),
+				Duration: time.Minute,
+			},
+		},
+
+		ApplyEffects: func(sim *core.Simulation, _ *core.Unit, _ *core.Spell) {
+			frost.frozenOrb.EnableWithTimeout(sim, frost.frozenOrb, time.Second*10)
+		},
+	})
+
+	mage.AddMajorCooldown(core.MajorCooldown{
+		Spell: frozenOrb,
+		Type:  core.CooldownTypeDPS,
+	})
+}
+
+type FrozenOrb struct {
+	core.Pet
+
+	mageOwner *FrostMage
+
+	FrozenOrbTick *core.Spell
+
+	TickCount int64
+}
+
+func (frost *FrostMage) NewFrozenOrb() *FrozenOrb {
+	frozenOrb := &FrozenOrb{
+		Pet: core.NewPet(core.PetConfig{
+			Name:            "Frozen Orb",
+			Owner:           &frost.Character,
+			BaseStats:       frozenOrbBaseStats,
+			StatInheritance: createFrozenOrbInheritance(),
+			EnabledOnStart:  false,
+			IsGuardian:      true,
+		}),
+		mageOwner: frost,
+		TickCount: 0,
+	}
+
+	frozenOrb.Pet.OnPetEnable = frozenOrb.enable
+
+	mage.AddPet(frozenOrb)
+
+	return frozenOrb
+}
+
+func (frozenOrb *FrozenOrb) enable(sim *core.Simulation) {
+
+	frozenOrb.PseudoStats.DamageDealtMultiplier = frozenOrb.Owner.PseudoStats.DamageDealtMultiplier
+	frozenOrb.PseudoStats.SchoolDamageDealtMultiplier[stats.SchoolIndexFrost] = frozenOrb.Owner.PseudoStats.SchoolDamageDealtMultiplier[stats.SchoolIndexFrost]
+
+	frozenOrb.EnableDynamicStats(func(ownerStats stats.Stats) stats.Stats {
+		return stats.Stats{
+			stats.SpellPower: ownerStats[stats.SpellPower],
+		}
+	})
+}
+
+func (frozenOrb *FrozenOrb) GetPet() *core.Pet {
+	return &frozenOrb.Pet
+}
+
+func (frozenOrb *FrozenOrb) Initialize() {
+	frozenOrb.registerFrozenOrbTickSpell()
+}
+
+func (frozenOrb *FrozenOrb) Reset(_ *core.Simulation) {
+	frozenOrb.TickCount = 0
+}
+
+func (frozenOrb *FrozenOrb) ExecuteCustomRotation(sim *core.Simulation) {
+	spell := frozenOrb.FrozenOrbTick
+	if success := spell.Cast(sim, frozenOrb.CurrentTarget); !success {
+		frozenOrb.Disable(sim)
+	}
+}
+
+var frozenOrbBaseStats = stats.Stats{}
+
+var createFrozenOrbInheritance = func() func(stats.Stats) stats.Stats {
+	return func(ownerStats stats.Stats) stats.Stats {
+		return stats.Stats{
+			stats.SpellHitPercent:  ownerStats[stats.SpellHitPercent],
+			stats.SpellCritPercent: ownerStats[stats.SpellCritPercent],
+			stats.SpellPower:       ownerStats[stats.SpellPower],
+		}
+	}
+}
+
+// Values found at https://wago.tools/db2/SpellEffect?build=5.5.0.60802&filter%5BSpellID%5D=84721
+var frozenOrbCoefficient = 0.65
+var frozenOrbScaling = 0.51
+var frozenOrbVariance = 0.25
+
+func (frozenOrb *FrozenOrb) registerFrozenOrbTickSpell() {
+	frozenOrb.FrozenOrbTick = frozenOrb.RegisterSpell(core.SpellConfig{
+		ActionID:       core.ActionID{SpellID: 84721},
+		SpellSchool:    core.SpellSchoolFrost,
+		ProcMask:       core.ProcMaskSpellDamage,
+		ClassSpellMask: MageSpellFrozenOrb,
+
+		Cast: core.CastConfig{
+			DefaultCast: core.Cast{
+				GCD: time.Second,
+			},
+		},
+
+		DamageMultiplier: 1,
+		CritMultiplier:   frozenOrb.mageOwner.DefaultCritMultiplier(),
+		BonusCoefficient: frozenOrbCoefficient,
+		ThreatMultiplier: 1,
+		ApplyEffects: func(sim *core.Simulation, target *core.Unit, spell *core.Spell) {
+
+			damage := frozenOrb.mageOwner.CalcAndRollDamageRange(sim, frozenOrbScaling, frozenOrbVariance)
+			spell.CalcAndDealDamage(sim, target, damage, spell.OutcomeMagicHitAndCrit)
+
+			frozenOrb.TickCount += 1
+			if frozenOrb.TickCount == 10 {
+				frozenOrb.TickCount = 0
+			}
+		},
+	})
+}
