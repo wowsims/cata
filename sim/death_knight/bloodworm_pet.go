6a2af65f1878de1935cb5d26cec35b1124594d0f
Johan Hillerström <progr@mmer.nu>
2025-06-11 17:25:57 +0200

    [Blood] Change Blood Burst proc chance formula to incorporate dk health

--- sim/death_knight/bloodworm_pet.go
+++ sim/death_knight/bloodworm_pet.go
@@ -1,6 +1,8 @@
 package death_knight
 
 import (
+	"math"
+
 	"github.com/wowsims/mop/sim/core"
 	"github.com/wowsims/mop/sim/core/proto"
 	"github.com/wowsims/mop/sim/core/stats"
@@ -40,7 +42,7 @@ func (dk *DeathKnight) NewBloodwormPet(_ int) *BloodwormPet {
 
 	bloodworm.OnPetDisable = bloodworm.disable
 
-	// command doesn't apply to Bloodworms
+	// Command doesn't apply to Bloodworms
 	if dk.Race == proto.Race_RaceOrc {
 		bloodworm.PseudoStats.DamageDealtMultiplier /= 1.02
 	}
@@ -54,18 +56,38 @@ func (bloodworm *BloodwormPet) GetPet() *core.Pet {
 	return &bloodworm.Pet
 }
 
+func (bloodworm *BloodwormPet) getBloodBurstProcChance() float64 {
+	stacks := bloodworm.stackAura.GetStacks()
+	dkHealth := bloodworm.dkOwner.CurrentHealthPercent()
+	baseProcChance := math.Pow(float64(stacks+1), 3)
+	multiplier := 0.0
+
+	if dkHealth >= 100 && stacks >= 5 {
+		multiplier = 0.5
+	} else if dkHealth > 60 && dkHealth < 100 {
+		multiplier = 1.0
+	} else if dkHealth > 30 && dkHealth <= 60 {
+		multiplier = 1.5
+	} else if dkHealth <= 30 {
+		multiplier = 2.0
+	}
+
+	return baseProcChance * multiplier
+}
+
 func (bloodworm *BloodwormPet) Initialize() {
 	bloodworm.stackAura = bloodworm.GetOrRegisterAura(core.Aura{
-		Label:     "Blood Gorged Proc",
+		Label:     "Blood Gorged" + bloodworm.Label,
 		ActionID:  core.ActionID{SpellID: 81277},
 		Duration:  core.NeverExpires,
-		MaxStacks: 12,
+		MaxStacks: 99,
 	})
 
 	healSpell := bloodworm.dkOwner.GetOrRegisterSpell(core.SpellConfig{
 		ActionID:    core.ActionID{SpellID: 81280},
 		SpellSchool: core.SpellSchoolShadow,
 		ProcMask:    core.ProcMaskSpellHealing,
+		Flags:       core.SpellFlagPassiveSpell | core.SpellFlagHelpful,
 
 		DamageMultiplier: 1,
 		ThreatMultiplier: 1,
@@ -75,13 +97,16 @@ func (bloodworm *BloodwormPet) Initialize() {
 		ActionID:    core.ActionID{SpellID: 81280},
 		SpellSchool: core.SpellSchoolShadow,
 		ProcMask:    core.ProcMaskSpellHealing,
+		Flags:       core.SpellFlagPassiveSpell | core.SpellFlagHelpful,
 
 		DamageMultiplier: 1,
+		CritMultiplier:   bloodworm.DefaultCritMultiplier(),
 		ThreatMultiplier: 1,
 
 		ApplyEffects: func(sim *core.Simulation, target *core.Unit, spell *core.Spell) {
 			healSpell.Cast(sim, target)
 
+			baseHealing := float64(bloodworm.stackAura.GetStacks()) * 0.25 * bloodworm.MaxHealth()
 			for _, target := range sim.Raid.AllPlayerUnits {
 				if target == &bloodworm.Unit {
 					continue
@@ -91,22 +116,22 @@ func (bloodworm *BloodwormPet) Initialize() {
 					continue
 				}
 
-				wormHp := 4000 + (bloodworm.dkOwner.GetStat(stats.Stamina)-10)*3.45100
-				healSpell.CalcAndDealHealing(sim, target, float64(bloodworm.stackAura.GetStacks())*0.05*wormHp, healSpell.OutcomeHealing)
+				healSpell.CalcAndDealHealing(sim, target, baseHealing, healSpell.OutcomeHealingCrit)
 			}
+
 			bloodworm.Pet.Disable(sim)
 		},
 	})
 
-	explodeChances := []float64{0, 0, 0, 0.005, 0.005, 0.11, 0.18, 0.27, 0.36, 0.49, 0.75, 0.85, 1}
 	core.MakeProcTriggerAura(&bloodworm.Unit, core.ProcTrigger{
-		Name:     "Blood Gorged",
+		Name:     "Blood Gorged Trigger" + bloodworm.Label,
+		ActionID: core.ActionID{SpellID: 50453},
 		Callback: core.CallbackOnSpellHitDealt,
 		Outcome:  core.OutcomeLanded,
+
 		Handler: func(sim *core.Simulation, spell *core.Spell, result *core.SpellResult) {
-			if bloodworm.stackAura.IsActive() && bloodworm.stackAura.GetStacks() >= 3 {
-				explodeChance := explodeChances[bloodworm.stackAura.GetStacks()]
-				if sim.Proc(explodeChance, "Blood Burst") {
+			if bloodworm.stackAura.IsActive() {
+				if bloodworm.getBloodBurstProcChance() > sim.RollWithLabel(0, 999, "Blood Burst"+bloodworm.Label) {
 					explosion.Cast(sim, &bloodworm.Unit)
 					return
 				}
@@ -140,10 +165,10 @@ func (dk *DeathKnight) bloodwormStatInheritance() core.PetStatInheritance {
 			stats.CritRating:          ownerStats[stats.CritRating],
 			stats.ExpertiseRating:     combined,
 			stats.HasteRating:         ownerStats[stats.HasteRating],
-			stats.Health:              ownerStats[stats.Health],
+			stats.Health:              ownerStats[stats.Health] * 0.15,
 			stats.HitRating:           combined,
 			stats.PhysicalCritPercent: ownerStats[stats.PhysicalCritPercent],
-			stats.Stamina:             ownerStats[stats.Stamina],
+			stats.Stamina:             ownerStats[stats.Stamina] * 0.15,
 		}
 	}
 }
