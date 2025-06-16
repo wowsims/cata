ae1b9bbd8a10efededb1f80ccdfbb018a324c8f8
Adrian Klingen <adrian.klingen@deptagency.com>
2025-06-16 22:30:55 +0200

    Add SpellFlag check to SpellMod

--- sim/core/spell_mod.go
+++ sim/core/spell_mod.go
@@ -36,6 +36,7 @@ type SpellMod struct {
 	Kind           SpellModType
 	School         SpellSchool
 	ProcMask       ProcMask
+	SpellFlag      SpellFlag
 	ResourceType   proto.ResourceType
 	floatValue     float64
 	intValue       int32
@@ -92,6 +93,7 @@ func buildMod(unit *Unit, config SpellModConfig) *SpellMod {
 		Kind:         config.Kind,
 		School:       config.School,
 		ProcMask:     config.ProcMask,
+		SpellFlag:    config.SpellFlag,
 		ResourceType: config.ResourceType,
 		floatValue:   config.FloatValue,
 		intValue:     config.IntValue,
@@ -179,6 +181,10 @@ func shouldApply(spell *Spell, mod *SpellMod) bool {
 	}
 
 	if mod.ProcMask > 0 && !mod.ProcMask.Matches(spell.ProcMask) {
+		return false
+	}
+
+	if mod.SpellFlag > 0 && !mod.SpellFlag.Matches(spell.Flags) {
 		return false
 	}
 
