2d75b3138b9b3a9c22086efc297d272377d03394
Polynomix <Polynomix@protonmail.com>
2025-06-19 21:11:53 +0200

    fix LvB damage

--- sim/shaman/elemental/lavaburst.go
+++ sim/shaman/elemental/lavaburst.go
@@ -67,12 +67,15 @@ func (ele *ElementalShaman) newLavaBurstSpellConfig(isElementalOverload bool) co
 	}
 
 	spellConfig.ApplyEffects = func(sim *core.Simulation, target *core.Unit, spell *core.Spell) {
+		var result *core.SpellResult
 		baseDamage := ele.CalcAndRollDamageRange(sim, 1.41624999046, 0.10000000149)
 		if ele.FlameShock.Dot(target).IsActive() {
-			baseDamage *= 1.5
+			spell.DamageMultiplier *= 1.5
+			result = spell.CalcDamage(sim, target, baseDamage, spell.OutcomeMagicHitAndCrit)
+			spell.DamageMultiplier /= 1.5
+		} else {
+			result = spell.CalcDamage(sim, target, baseDamage, spell.OutcomeMagicHitAndCrit)
 		}
-		result := spell.CalcDamage(sim, target, baseDamage, spell.OutcomeMagicHitAndCrit)
-
 		idx := core.TernaryInt32(spell.Flags.Matches(shaman.SpellFlagIsEcho), 1, 0)
 		spell.WaitTravelTime(sim, func(sim *core.Simulation) {
 			if !isElementalOverload && result.Landed() && sim.Proc(ele.GetOverloadChance(), "Lava Burst Elemental Overload") {
