314e1a00c6df2c5d1e85f634acea5668fa35736b
Johan Hillerström <progr@mmer.nu>
2025-06-22 15:31:41 +0200

    [Blood] Add tooltip to WotN

--- sim/death_knight/blood/veteran_of_the_third_war.go
+++ sim/death_knight/blood/veteran_of_the_third_war.go
@@ -7,7 +7,7 @@ import (
 
 // Increases your total Stamina by 9% and your chance to dodge by 2%.
 func (bdk *BloodDeathKnight) registerVeteranOfTheThirdWar() {
-	core.MakePermanent(bdk.GetOrRegisterAura(core.Aura{
+	core.MakePermanent(bdk.RegisterAura(core.Aura{
 		Label:    "Veteran of the Third War" + bdk.Label,
 		ActionID: core.ActionID{SpellID: 50029},
 	})).AttachMultiplicativePseudoStatBuff(
